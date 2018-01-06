41acd7e5800ac3fd0cbba8c2d24172181340c1d7
Karolina <->
2018-01-06 15:15:10 +0100

    changed mapping method

--- spring-boot-server/src/main/kotlin/cs/io/buildingInfo/location/LocationCalculationsController.kt
+++ spring-boot-server/src/main/kotlin/cs/io/buildingInfo/location/LocationCalculationsController.kt
@@ -1,18 +1,19 @@
 package cs.io.buildingInfo.location
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class LocationCalculationsController {
-  @GetMapping("/cube")
+  @PostMapping("/cube")
   fun countTotalCube(@RequestBody loc: Location): Double {
     return loc.cube
   }
 
-  @GetMapping("/area")
+  @PostMapping("/area")
   fun countTotalArea(@RequestBody loc: Location): Double {
-    return loc.cube
+    return loc.area
   }
 }
