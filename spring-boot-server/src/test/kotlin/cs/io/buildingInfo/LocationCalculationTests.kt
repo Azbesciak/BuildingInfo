c879ff232209a8763cc092c5ebf07d734d0047af
KarolinJ <karolinaj96@op.pl>
2018-01-23 16:06:55 +0100

    New tests

--- spring-boot-server/src/test/kotlin/cs/io/buildingInfo/LocationCalculationTests.kt
+++ spring-boot-server/src/test/kotlin/cs/io/buildingInfo/LocationCalculationTests.kt
@@ -27,7 +27,7 @@ class LocationCalculationTests(private val webApplicationContext: WebApplication
 
 
   @Test
-  fun `test area calculations for building`() {
+  fun `test cube calculations for building`() {
     val perform = mockMvc.perform(post("/cube").contentType(MediaType.APPLICATION_JSON_UTF8)
       .content("""
         {
@@ -84,5 +84,187 @@ class LocationCalculationTests(private val webApplicationContext: WebApplication
     perform
       .andExpect(status().isOk)
       .andExpect(content().json("138.0"))
+  }
+
+  @Test
+  fun `test area calculations for building`() {
+    val perform = mockMvc.perform(post("/area").contentType(MediaType.APPLICATION_JSON_UTF8)
+      .content("""
+        {
+          "id": 7,
+          "name": "some building",
+          "levels": [
+            {
+              "id": 14,
+              "name": "1st",
+              "rooms": [
+                {
+                  "cube": 17,
+                  "area": 8,
+                  "heating": 17,
+                  "light": 18,
+                  "id": 2,
+                  "name": "some name"
+                },
+                {
+                  "cube": 16,
+                  "area": 4,
+                  "heating": 12,
+                  "light": 23,
+                  "id": 14,
+                  "name": "some name"
+                }
+              ]
+            },
+            {
+              "id": 69,
+              "name": "2st",
+              "rooms": [
+                {
+                  "cube": 89,
+                  "area": 8,
+                  "heating": 17,
+                  "light": 18,
+                  "id": 21,
+                  "name": "some name"
+                },
+                {
+                  "cube": 16,
+                  "area": 4,
+                  "heating": 12,
+                  "light": 23,
+                  "id": 141,
+                  "name": "some name"
+                }
+              ]
+            }
+          ]
+        }
+        """)).andDo(MockMvcResultHandlers.print())
+    perform
+      .andExpect(status().isOk)
+      .andExpect(content().json("24.0"))
+  }
+
+
+
+  @Test
+  fun `test lightPerArea calculations for building`() {
+    val perform = mockMvc.perform(post("/lightPerArea").contentType(MediaType.APPLICATION_JSON_UTF8)
+      .content("""
+        {
+          "id": 7,
+          "name": "some building",
+          "levels": [
+            {
+              "id": 14,
+              "name": "1st",
+              "rooms": [
+                {
+                  "cube": 17,
+                  "area": 8,
+                  "heating": 17,
+                  "light": 18,
+                  "id": 2,
+                  "name": "some name"
+                },
+                {
+                  "cube": 16,
+                  "area": 4,
+                  "heating": 12,
+                  "light": 23,
+                  "id": 14,
+                  "name": "some name"
+                }
+              ]
+            },
+            {
+              "id": 69,
+              "name": "2st",
+              "rooms": [
+                {
+                  "cube": 89,
+                  "area": 8,
+                  "heating": 17,
+                  "light": 18,
+                  "id": 21,
+                  "name": "some name"
+                },
+                {
+                  "cube": 16,
+                  "area": 4,
+                  "heating": 12,
+                  "light": 25,
+                  "id": 141,
+                  "name": "some name"
+                }
+              ]
+            }
+          ]
+        }
+        """)).andDo(MockMvcResultHandlers.print())
+    perform
+      .andExpect(status().isOk)
+      .andExpect(content().json("3.5"))
+  }
+
+  @Test
+  fun `test heatPerCube calculations for building`() {
+    val perform = mockMvc.perform(post("/heatPerCube").contentType(MediaType.APPLICATION_JSON_UTF8)
+      .content("""
+        {
+          "id": 7,
+          "name": "some building",
+          "levels": [
+            {
+              "id": 14,
+              "name": "1st",
+              "rooms": [
+                {
+                  "cube": 17,
+                  "area": 8,
+                  "heating": 13,
+                  "light": 18,
+                  "id": 2,
+                  "name": "some name"
+                },
+                {
+                  "cube": 16,
+                  "area": 4,
+                  "heating": 12,
+                  "light": 23,
+                  "id": 14,
+                  "name": "some name"
+                }
+              ]
+            },
+            {
+              "id": 69,
+              "name": "2st",
+              "rooms": [
+                {
+                  "cube": 81,
+                  "area": 8,
+                  "heating": 15,
+                  "light": 18,
+                  "id": 21,
+                  "name": "some name"
+                },
+                {
+                  "cube": 16,
+                  "area": 4,
+                  "heating": 12,
+                  "light": 25,
+                  "id": 141,
+                  "name": "some name"
+                }
+              ]
+            }
+          ]
+        }
+        """)).andDo(MockMvcResultHandlers.print())
+    perform
+      .andExpect(status().isOk)
+      .andExpect(content().json("0.4"))
   }
 }
